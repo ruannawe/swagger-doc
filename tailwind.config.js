e5dbab5fc315f9e60db7395bdf3fd0fa9b6ff53b
Ruan Herculano <ruannawe95@outlook.com>
2022-10-16 17:00:24 -0300

    tailwind with postcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+//    require('@tailwindcss/forms'),
+//    require('@tailwindcss/typography'),
+//    require('@tailwindcss/aspect-ratio'),
+//    require('@tailwindcss/line-clamp'),
+  ]
+}
